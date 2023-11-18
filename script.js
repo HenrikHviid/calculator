dbd364c036555e2d3969f54f8774baab90b4a1fe
Henrik Hviid <hhhansen@gmail.com>
2023-11-18 21:40:12 +0100

    [ADD] Functionality to copy/paste several values

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const display = document.getElementById('display');
     const buttonsContainer = document.querySelector('.buttons');
 
-    let memory1 = 0;
-    let memory2 = 0;
+    let memories = new Array(10).fill('');
+    let selectedMemorySlot = null;
     let calculationResult = false;
 
     // Funktion til at tilføje en knap
@@ -30,9 +30,11 @@ document.addEventListener('DOMContentLoaded', function () {
             case 'CLR':
                 clearDisplay();
                 break;
-            case 'M1':
-            case 'M2':
-                saveToMemory(value);
+            case 'M':
+                saveToMemory();
+                break;
+            case 'R':
+                showMemoryPopup();
                 break;
             default:
                 addToDisplay(value);
@@ -42,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Funktion til at evaluere udtrykket
     function evaluateExpression() {
         display.value = eval(display.value);
-        calculationResult = true // sætter variabel til tegn ikke må slettes
+        calculationResult = true; // sætter variabel til tegn ikke må slettes
     }
 
     // Funktion til at slette det sidste tegn
@@ -53,6 +55,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (calculationResult) {
             return;
         }
+
         // Slet det sidste tegn
         display.value = currentExpression.slice(0, -1);
     }
@@ -63,39 +66,72 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Funktion til at gemme værdi i hukommelse
-    function saveToMemory(memoryType) {
-        const memoryValue = display.value;
-        if (memoryType === 'M1') {
-            memory1 = memoryValue;
-        } else {
-            memory2 = memoryValue;
+    function saveToMemory() {
+        const inputValue = display.value;
+
+        // Automatisk vælg den næste ledige hukommelsesplads
+        selectedMemorySlot = memories.indexOf('');
+        if (selectedMemorySlot === -1) {
+            alert('Alle hukommelsespladser er fyldte.');
+            return;
         }
+
+        memories[selectedMemorySlot] = inputValue;
+        alert(`Værdien er gemt i hukommelse ${selectedMemorySlot + 1}`);
+    }
+
+    // Funktion til at vise popup med gemte værdier og lade brugeren vælge
+    function showMemoryPopup() {
+        const memoryOptions = memories.map((value, index) => ({
+            label: `Hukommelse ${index + 1}: ${value}`,
+            value: index
+        }));
+
+        const selectedOption = window.prompt('Vælg en gemt værdi:\n\n' +
+            memoryOptions.map(option => `${option.label}`).join('\n'));
+
+        if (selectedOption !== null && !isNaN(selectedOption)) {
+            selectedMemorySlot = parseInt(selectedOption, 10);
+            retrieveFromMemory();
+        }
+    }
+
+    // Funktion til at hente værdi fra hukommelse
+    function retrieveFromMemory() {
+        // Tjek om der er valgt en hukommelsesplads
+        if (selectedMemorySlot === null || isNaN(selectedMemorySlot) ||
+            selectedMemorySlot < 0 || selectedMemorySlot >= memories.length) {
+            alert('Ugyldig hukommelsesplads. Hentning annulleret.');
+            return;
+        }
+
+        display.value += memories[selectedMemorySlot];
     }
 
     // Funktion til at tilføje værdi til displayet
     function addToDisplay(value) {
         const currentExpression = display.value;
         const lastCharacter = currentExpression.slice(-1);
-    
+
         // Tjek om displayet er tomt, og det indtastede tegn er en af de forbudte operatører
         if (currentExpression === '' && ['+', '/', '*'].includes(value)) {
             // Ignorer tilføjelsen
             return;
         }
-    
+
         // Tjek om det sidste tegn er resultatet af en udregning
         if (calculationResult && !['+', '-', '*', '/'].includes(value)) {
             // Ignorer tilføjelsen, hvis det er et tal efter en udregning
             return;
         }
-    
+
         display.value += value;
         calculationResult = false; // Nulstil variablen, når der tilføjes en ny værdi
     }
 
     // Tilføjede knapper: M1, M2, C, CLR, osv.
     const buttons = [
-        'M1', 'M2', 'C', 'CLR',
+        'M', 'R', 'C', 'CLR',
         '7', '8', '9', '/',
         '4', '5', '6', '*',
         '1', '2', '3', '-',
@@ -105,7 +141,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Tilføj knapper ved at iterere over knapværdierne
     buttons.forEach(buttonValue => {
         // Tilføj ekstra klasse til 'M1' og 'M2' knapper
-        const specialClass = buttonValue === 'M1' || buttonValue === 'M2' ? 'special' : '';
+        const specialClass = buttonValue === 'M' || buttonValue === 'R' ? 'special' : '';
         addButton(buttonValue, specialClass);
     });
 });
